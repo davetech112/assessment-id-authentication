f0a69c4ef14d416a4423e5d639339cbec93b378c
dev <davidonyeka@initsng.com>
2025-10-03 20:40:58 +0100

    Initial Spring Boot setup for MOSIP assessment

---
+++ src/main/java/io/mosip/id_authentication_assessment/IdAuthenticationAssessmentApplication.java
@@ -0,0 +1,13 @@
+package io.mosip.id_authentication_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdAuthenticationAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdAuthenticationAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mosip/id_authentication_assessment/IdAuthenticationAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mosip.id_authentication_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdAuthenticationAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
